9d028211a0e6b31724f6c7d56e3339e6fa6f1858
Leandro Ribeiro <github@leandroribeiro.com>
2017-07-08 19:05:08 -0300

    Done lesson 12

---
+++ vuejs1.x/lesson12/main.js
@@ -0,0 +1,28 @@
+Vue.component('alert', {
+    template: '#alert-template',
+
+    props: ['type'],
+
+    data: function(){
+        return {
+            show: true
+        }
+    },
+
+    computed:{
+        alertClasses: function(){
+            var type = this.type;
+
+            return{
+                'alert': true,
+                'alert-success': type == 'success',
+                'alert-error': type == 'error'
+            }
+        }
+    }
+
+});
+
+new Vue({
+    el: 'body'
+});
