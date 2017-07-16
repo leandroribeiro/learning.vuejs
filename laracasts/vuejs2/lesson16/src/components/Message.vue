580a63dc0aca53753f3371f6e4f2584394b44a73
Leandro Ribeiro <github@leandroribeiro.com>
2017-07-16 20:09:34 -0300

    Done lesson 16

---
+++ laracasts/vuejs2/lesson16/src/components/Message.vue
@@ -0,0 +1,22 @@
+<template lang="html">
+  <div class="box">
+    <p>
+        <slot></slot>
+    </p>
+
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="css">
+.box{
+  background-color: gray;
+  padding: 10px;
+  border: 1px solid black;
+  margin-bottom: 1em;
+}
+</style>
