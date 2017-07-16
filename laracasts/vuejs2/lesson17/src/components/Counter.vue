3fc3faf434c80205abff09f0839998dbc60e2499
Leandro Ribeiro <github@leandroribeiro.com>
2017-07-16 20:10:34 -0300

    Done lesson 17

---
+++ laracasts/vuejs2/lesson17/src/components/Counter.vue
@@ -0,0 +1,24 @@
+<template lang="html">
+  <div class="">
+    <h2>
+      Count: {{ count }}
+    </h2>
+
+    <p>
+      <button type="button" name="button" @click="count += 1">Increment</button>
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      count: 0
+    };
+  }
+}
+</script>
+
+<style lang="css">
+</style>
