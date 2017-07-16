f927eb7326ea39f756c135b5e51a4ae61d5fe25d
Leandro Ribeiro <github@leandroribeiro.com>
2017-07-16 20:11:48 -0300

    Done lesson 18

---
+++ laracasts/vuejs2/lesson18-netcore/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace lesson18_netcore.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("api/skills")]
+        [HttpGet]
+        public IEnumerable<string> Skills()
+        {
+            return new string[]{ "netcore", "aspnet", "vue", "javascript" };
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
---
+++ laracasts/vuejs2/lesson18-netcore/wwwroot/js/site.js
@@ -0,0 +1,11 @@
+﻿new Vue({
+    el: '#app',
+
+    data: {
+        skills: []
+    },
+
+    mounted() {
+        axios.get('/api/skills').then(response => this.skills = response.data);
+    }
+});
