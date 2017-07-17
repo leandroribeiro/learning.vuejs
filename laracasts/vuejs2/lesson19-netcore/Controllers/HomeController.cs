a8ee3ab065a7d74ee16803e476533594ca15b221
Leandro Ribeiro <github@leandroribeiro.com>
2017-07-16 21:39:42 -0300

    Implementing lesson 19

---
+++ laracasts/vuejs2/lesson19-netcore/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using lesson19_netcore.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace lesson19_netcore.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(string name, string description)
+        {
+            //validate
+
+            //save on database
+
+            return Json(new { response = "Project Created!" });
+            //return this.Ok(new { response = "Project Created!" });
+        }
+    }
+}
